93cd6f4abd4353d7a5b9318c1af4535ebca1c709
Túlio César Feldmann <90226273+tuliocesarfeldmann@users.noreply.github.com>
2022-07-26 18:38:37 -0300

    Delete Aula0707 directory

--- Aula0707/no.py
+++
@@ -1,8 +0,0 @@
-class No:
-    def __init__(self, key, dir, esq):
-        self.item = key
-        self.dir = dir
-        self.esq = esq
-
-    def __repr__(self) -> str:
-        return str(self.item)
